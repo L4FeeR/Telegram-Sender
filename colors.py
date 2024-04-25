ccb5d97274020e6edb404d9ced916d3d583e56b2
Muhamed Lafeer <l4feer@Evolver>
2024-04-26 04:08:42 +0530

    added color config file.

---
+++ colors.py
@@ -0,0 +1,17 @@
+#colors configuration
+
+reset="\033[0;0m"
+red="\033[1;31m"
+cyan="\033[1;32m"
+yellow="\033[1;93m"
+blue="\033[1;34m"
+purple="\033[1;35m"
+green="\033[1;36m"
+white="\033[1;37m"
+hide="\033[1;08m"
+strike="\033[1;09m"
+bg="\033[1;07m"
+blink="\033[1;05m"
+italic="\033[1;03m"
+light="\033[1;02m"
+underline="\033[1;04m"
